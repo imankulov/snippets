31338d3bfa2a196e81f9ba04fe29f637f13e43f5
Roman Imankulov <roman.imankulov@gmail.com>
2021-12-04 18:14:42 +0000

    Update poetry-init.sh

--- poetry-init.sh
+++ poetry-init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # quickly initialize a new Python environment with Poetry
-poetry init -n
+#
+# We explicitly stick to Python 3.10.x (we don't want automatic upgrades to 3.11)
+poetry init --python '~3.10' --no-interaction --quiet
 poetry install
 echo 'source $(poetry env info -p)/bin/activate' > .startup
